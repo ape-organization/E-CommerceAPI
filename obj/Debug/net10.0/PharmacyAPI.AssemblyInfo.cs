30c67a1dbfff06295b6bc2626f4d85eacd061b0f
POlaNasserSamouel <121816986+POlaNasserSamouel@users.noreply.github.com>
2026-08-31 18:25:42 +0300

    change connection string

--- obj/Debug/net10.0/PharmacyAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/PharmacyAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmacyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0+eed5ee371b3e236175b9c8777f8d8a4321f6b473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0+970a278e0d948c4c231efcdfbe08be82f483aeb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmacyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmacyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
