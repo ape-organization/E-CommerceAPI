a82f202eeace6380f6591966b4f15528f8df81d4
POlaNasserSamouel <121816986+POlaNasserSamouel@users.noreply.github.com>
2026-09-03 21:01:37 +0300

    temp

--- Services/ImageService.cs
+++ Services/ImageService.cs
@@ -54,6 +54,6 @@ public class ImageService
             stream,
             cancellationToken);
 
-        return $"/uploads/{folder}/{fileName}";
+        return $"/uploads/E_Commerce/{folder}/{fileName}";
     }
 }
